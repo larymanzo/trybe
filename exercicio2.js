c62843f94fa49fccc8d7254bfb95253854a7d400
larymanzo <lamosinha@hotmail.com>
2021-07-05 21:41:40 -0300

    exercicio 2

---
+++ exercicio2.js
@@ -0,0 +1,17 @@
+// Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Copie o código abaixo.
+
+const sortOddsAndEvens = () => {
+    oddsAndEvens[0] = 2;
+    oddsAndEvens[1] = 3;
+    oddsAndEvens[2] = 4;
+    oddsAndEvens[3] = 7;
+    oddsAndEvens[4] = 10;
+    oddsAndEvens[5] = 13;
+
+    return oddsAndEvens;
+}
+
+const sortedArray = sortOddsAndEvens();
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
